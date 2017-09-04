6aaaf554f47acb6d30851664555f439a8835f34a
Gerben Rampaart <rampaart@gmail.com>
2017-09-04 08:05:46 +0200

    Commit 10:46 on video

--- src/app.js
+++ src/app.js
@@ -1,4 +1,34 @@
 import $ from 'jquery';
 import Rx from 'rxjs/Rx';
 
-console.log('RxJS Boiler Running...');
+const btn = $('#btn');
+
+const btnStream$ = Rx.Observable
+    .fromEvent(btn, 'click');
+
+btnStream$.subscribe(
+    function(e) {
+        console.log(e);
+    },
+    function(err) {
+        console.log(err);
+    },
+    function() {
+        console.log('completed');
+    }
+);
+
+const inputStream$ = Rx.Observable
+    .fromEvent(input, 'keyup');
+
+inputStream$.subscribe(
+function(e) {
+    console.log(e.target.value);
+},
+function(err) {
+    console.log(err);
+},
+function() {
+    console.log('completed');
+}
+);
